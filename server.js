a60fbc88a9be25db02217a6b322b45277b5b567a
shadowmoonezios <agushifelde@hotmail.com>
2025-09-27 15:23:11 +0300

    Updated MongoDB connection URI for staging environment.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // MongoDB connection
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/urlaubplatzfinder';
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/urlaubplatzfinder-staging';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
