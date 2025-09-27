70ac6ff93430186677316f8327eefe7147c273aa
shadowmoonezios <agushifelde@hotmail.com>
2025-09-27 15:23:48 +0300

    Fix console log message for MongoDB connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // MongoDB connection
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/urlaubplatzfinder-staging';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
     process.exit(1);
