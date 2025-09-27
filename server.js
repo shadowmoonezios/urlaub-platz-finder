7fb28beefa4f4c2b13c05a1f84f044b130f9bf18
shadowmoonezios <agushifelde@hotmail.com>
2025-09-27 15:21:53 +0300

    Update MongoDB connection options to remove deprecated useFindAndModify and useCreateIndex

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // MongoDB connection
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/urlaubplatzfinder_db';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
